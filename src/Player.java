a4c7efe903d5e75ddf3f777d76049ed6498aec00
felixed <felixed@kth.se>
2016-05-26 17:10:14 +0200

    Player class added fields, constructor and setters/getters for the fields

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,88 @@
+import java.util.HashMap;
+import java.util.Stack;
 
+/**
+ * The Player class stores information regarding the player.
+ * The player is created when the user starts the game.
+ * @author felix
+ *
+ */
 public class Player {
-
+	private String name;
+	private HashMap<String, Item> inventory;
+	private Room currentRoom;
+	private Stack<Room> roomHistory;
+	
+	/**
+	 * Constructor for the Player, sets the name for the user.
+	 * @param name The name of the player, used for leader boards.
+	 */
+	public Player(String name) {
+		this.name = name;
+		inventory = new HashMap<String, Item>();
+		roomHistory = new Stack<Room>();
+	}
+	
+	/**
+	 * @return The name of the player
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	//This method could be unnecessary.
+	/**
+	 * @return The inventory of the player
+	 */
+	public HashMap<String, Item> getInventory() {
+		return inventory;
+	}
+	
+	//Method in Item class will be added
+	/**
+	 * Adds an item to the inventory
+	 * @param item The item to be added
+	 */
+	public void setInventory(Item item) {
+		inventory.put(item.getName(), item);
+	}
+	
+	/**
+	 * Returns an item from the inventory
+	 * @param item The item to be returned
+	 * @return The item to be returned
+	 */
+	public Item getInventory(Item item) {
+		return inventory.get(item.getName());
+	}
+	
+	/**
+	 * @return The current room of the player
+	 */
+	public Room getCurrentRoom() {
+		return currentRoom;
+	}
+	
+	/**
+	 * Changes the current room of the player
+	 * @param currentRoom The current room of the player
+	 */
+	public void setCurrentRoom(Room currentRoom) {
+		this.currentRoom = currentRoom;
+	}
+	
+	/**
+	 * @return The latest room the player visited
+	 */
+	public Room getRoomHistory() {
+		return roomHistory.pop();
+	}
+	
+	/**
+	 * Adds a new room to the room history
+	 * @param room The latest room the player visited
+	 */
+	public void setRoomHistory(Room room) {
+		roomHistory.push(room);
+	}
 }
