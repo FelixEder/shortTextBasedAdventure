e581dcbd6aabb14b1542d92a6b064df41c88aebf
felixed <felixed@kth.se>
2016-05-27 10:25:58 +0200

    Fields added to parser and new class CommandWords added

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 
+/**
+ * The Parser class reads the player input and recognizes it as commands
+ * @author felix
+ *
+ */
 public class Parser {
-
+	private CommandWords commands;
+	private Scanner reader;
 }
