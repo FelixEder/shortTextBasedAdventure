e39c794b5ddf370f7c887815d8fb8565eda0b9c8
felixed <felixed@kth.se>
2016-05-29 12:07:08 +0200

    More functionality added to Game Class, can now be played from terminal

--- src/Game.java
+++ src/Game.java
@@ -29,7 +29,7 @@ public class Game {
 	/**
 	 * Creates all the different rooms in the game and sets all exits.
 	 */
-	public void createRooms() {
+	private void createRooms() {
 		
 	}
 	
@@ -37,6 +37,38 @@ public class Game {
 	 * Starts the infinite loop that is used when the game is running
 	 */
 	public void play() {
+		printStartMessage();
 		
+		boolean done = false;
+		while(!done) {
+			Command command = parser.getCommand();
+			done = processCommand(command);
+		}
+		System.out.println("Thanks for enjoying my little game, have a nice day.");
+	}
+	
+	/**
+	 * The text that is printed out in beginning of the game.
+	 */
+	private void printStartMessage() {
+		
+	}
+	
+	/**
+	 * Processes the commands from the player
+	 * @param command The command from the player
+	 * @return The boolean regarding if the player wants to quit the game
+	 */
+	private boolean processCommand(Command command) {
+		boolean wantToQuit = false;
+		enumCommands commandWord = command.getCommandWord();
+		
+		switch(commandWord) {
+		//Add more cases as the number of commands increases.
+		default:   
+			System.out.println("I don't know what you mean..." + "\n");
+            break;
+		}
+		return wantToQuit;
 	}
 }
