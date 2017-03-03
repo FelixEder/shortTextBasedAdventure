ab3a56335e794e497e6547d58dcd955906556b8b
felixed <felixed@kth.se>
2017-03-03 18:06:45 +0100

    Created new classes Collectibles and Element and will try to implement some kind of inheritance with abstract classes, therefor a new branch

---
+++ src/Collectibles.java
@@ -0,0 +1,13 @@
+/**
+ * Collectibles are similar to items, they can be picked up by the player
+ * but are then promptly removed from the game.
+ * @author felix
+ *
+ */
+public class Collectibles extends Element {
+
+	public Collectibles(String description) {
+		super.description = description;
+	}
+
+}
---
+++ src/Element.java
@@ -0,0 +1,9 @@
+
+public abstract class Element {
+	protected String description;
+	
+	public String getDescription() {
+		return description;
+	}
+	
+}
